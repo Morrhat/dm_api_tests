7499d3619829c93934beecec5913950b29261b04
Morrhat <kovengor@gmail.com>
2025-10-24 00:21:21 +0300

    Вынесли функции в классы клиенты

---
+++ dm_api_account/apis/account_api.py
@@ -0,0 +1,54 @@
+import requests
+
+
+class AccountAPI:
+    def __init__(
+            self,
+            host,
+            headers=None
+    ):
+        self.host = host
+        self.headers = headers
+
+    def post_v1_account(
+            self,
+            json_data
+    ):
+
+        """
+        POST
+        /v1/account
+        Register new user
+        :param json_data:
+        :return:
+        """
+
+
+        response = requests.post(
+            url=f'{self.host}/v1/account',
+            json=json_data
+        )
+        return response
+
+    def put_v1_account_token(
+            self,
+            token
+    ):
+
+        """
+        PUT
+        /v1/account/{token}
+        Activate registered user
+        :param token:
+        :return:
+        """
+
+
+        headers = {
+            'accept': 'text/plain',
+        }
+        response = requests.put(
+            url=f'{self.host}/v1/account/{token}',
+            headers=headers
+        )
+        return response
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,59 +1,48 @@
-import requests
-import pprint
 from json import loads
 
+from dm_api_account.apis.account_api import AccountAPI
+from dm_api_account.apis.login_api import LoginAPI
+from api_mailhog.apis.mailhog_api import MailhogAPI
+
+
 def test_post_v1_account():
-
-    login = 'kristinochka_test4'
-    password = '123456789'
-    email = f'{login}@mail.com'
-
     # Регистрация пользователя
 
+    account_api = AccountAPI(host='http://5.63.153.31:5051')
+    login_api = LoginAPI(host='http://5.63.153.31:5051')
+    mailhog_api = MailhogAPI(host='http://5.63.153.31:5025')
+
+    login = 'kristinochka_test8'
+    password = '123456789'
+    email = f'{login}@mail.com'
     json_data = {
         'login': login,
         'email': email,
         'password': password,
     }
 
-    response = requests.post('http://5.63.153.31:5051/v1/account', json=json_data)
+    response = account_api.post_v1_account(json_data=json_data)
     print(response.status_code)
     print(response.text)
     assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
 
-
-
-
     # Получить письма из почтового сервера
 
-    params = {
-        'limit': '10',
-    }
-
-    response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
+    response = mailhog_api.get_api_v2_messages()
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, f"Письма не были получены {response.json()}"
-  #  pprint.pprint(response.json())
-
-
+    #  pprint.pprint(response.json())
 
     # Получить активационный токен
-    token = None
-    for item in response.json()['items']:
-        user_data = loads(item['Content']['Body'])
-        user_login = user_data['Login']
+    token = get_activation_token_by_login(login, response)
 
-        if user_login == login:
-            print(user_login)
-            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-            print(token)
     assert token is not None, f"Токен для пользоваетля {login} не был получен"
-
 
     # Активация пользователя
 
-    response = requests.put(f'http://5.63.153.31:5051/v1/account/{token}')
+    response = account_api.put_v1_account_token(token=token)
+
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, f"Пользователь не был активирован {response.json()}"
@@ -66,8 +55,21 @@ def test_post_v1_account():
         'rememberMe': True,
     }
 
-    response = requests.post('http://5.63.153.31:5051/v1/account/login', json=json_data)
+    response = login_api.post_v1_account_login(json_data=json_data)
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, f"Пользователь не смог авторизоваться {response.json()}"
 
+
+def get_activation_token_by_login(
+        login,
+        response
+        ):
+    token = None
+    for item in response.json()['items']:
+        user_data = loads(item['Content']['Body'])
+        user_login = user_data['Login']
+
+        if user_login == login:
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+    return token
