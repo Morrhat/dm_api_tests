2f593f4e717455a093edbf8c73d2f9519fc66e63
Morrhat <kovengor@gmail.com>
2025-10-28 15:27:49 +0300

    Добавлен класс configuration

--- tests/functional/post_v1_account/test_post_v1_account_login.py
+++ tests/functional/post_v1_account/test_post_v1_account_login.py
@@ -3,6 +3,8 @@ from json import loads
 from dm_api_account.apis.account_api import AccountAPI
 from dm_api_account.apis.login_api import LoginAPI
 from api_mailhog.apis.mailhog_api import MailhogAPI
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
 import structlog
 
 structlog.configure(
@@ -19,11 +21,14 @@ structlog.configure(
 def test_post_v1_account_login():
     # Регистрация пользователя
 
-    account_api = AccountAPI(host='http://5.63.153.31:5051')
-    login_api = LoginAPI(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogAPI(host='http://5.63.153.31:5025')
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
-    login = 'kristinochka_test116'
+    account_api = AccountAPI(configuration=dm_api_configuration)
+    login_api = LoginAPI(configuration=dm_api_configuration)
+    mailhog_api = MailhogAPI(configuration=mailhog_configuration)
+
+    login = 'kristinochka_test126'
     password = '123456789'
     email = f'{login}@mail.com'
     json_data = {
--- tests/functional/post_v1_account/test_put_v1_account_token.py
+++ tests/functional/post_v1_account/test_put_v1_account_token.py
@@ -3,6 +3,8 @@ from json import loads
 from dm_api_account.apis.account_api import AccountAPI
 from dm_api_account.apis.login_api import LoginAPI
 from api_mailhog.apis.mailhog_api import MailhogAPI
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
 import structlog
 
 structlog.configure(
@@ -19,11 +21,14 @@ structlog.configure(
 def test_put_v1_account_token():
     # Регистрация пользователя
 
-    account_api = AccountAPI(host='http://5.63.153.31:5051')
-    login_api = LoginAPI(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogAPI(host='http://5.63.153.31:5025')
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
-    login = 'kristinochka_test118'
+    account_api = AccountAPI(configuration=dm_api_configuration)
+    login_api = LoginAPI(configuration=dm_api_configuration)
+    mailhog_api = MailhogAPI(configuration=mailhog_configuration)
+
+    login = 'kristinochka_test124'
     password = '123456789'
     email = f'{login}@mail.com'
     json_data = {
