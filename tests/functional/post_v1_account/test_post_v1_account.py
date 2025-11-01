7e1f750c92a53ac2e735f9ca910d9bc3fb4ec929
Morrhat <kovengor@gmail.com>
2025-11-01 13:37:01 +0300

    Добавлены fixture для создания пользователя

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+from collections import namedtuple
 from json import loads
+import pytest
 
 from helpers.account_helper import AccountHelper
 from restclient.configuration import Configuration as MailhogConfiguration
@@ -18,20 +21,41 @@ structlog.configure(
     ]
 )
 
-
-def test_post_v1_account():
-    # Регистрация пользователя
+@pytest.fixture
+def mailhog_api():
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    mailhog_client = MailHogApi(configuration=mailhog_configuration)
+    return mailhog_client
+
+@pytest.fixture
+def account_api():
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
-
     account = DMApiAccount(configuration=dm_api_configuration)
-    mailhog = MailHogApi(configuration=mailhog_configuration)
+    return account
 
-    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
+@pytest.fixture
+def account_helper(account_api, mailhog_api):
+    account_helper = AccountHelper(dm_account_api=account_api, mailhog=mailhog_api)
+    return account_helper
 
-    login = 'kristinochka_test197'
+@pytest.fixture
+def prepare_user():
+    now = datetime.now()
+    data = now.strftime("%d_%m_%Y_%H_%M_%S")
+    login = f'kristinochka_{data}'
     password = '123456789'
     email = f'{login}@mail.com'
+    User = namedtuple("User", ['login', 'password', 'email'])
+    user = User(login=login, password=password, email=email)
+    return user
+
+def test_post_v1_account(account_helper, prepare_user):
+    login = prepare_user.login
+    password = prepare_user.password
+    email = prepare_user.email
+
+    # Регистрация пользователя
     account_helper.register_new_user(login=login, password=password, email=email)
+
 
 
